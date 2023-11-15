2c7d146f0d77b20e748b74d6a10b74035d18c191
Edimar Cardoso <edimarlnx@gmail.com>
2023-11-15 19:03:49 -0300

    Adding pg adapter

--- server-default.js
+++ server-default.js
@@ -2,6 +2,8 @@ import { Server } from "socket.io";
 import { initReporting } from "./reporting.js";
 import { createServer } from "http";
 import { instrument } from "@socket.io/admin-ui";
+import { createAdapter } from "@socket.io/postgres-adapter";
+import pg from "pg";
 
 const PORT = process.env.PORT || 3000;
 const httpServer = createServer();
@@ -18,19 +20,39 @@ instrument(io, {
   mode: "development",
 });
 
+if (process.env.PG_HOST) {
+  const pool = new pg.Pool({
+    user: process.env.PG_USER || "postgres",
+    host: process.env.PG_HOST || "localhost",
+    database: process.env.PG_DB || "postgres",
+    password: process.env.PG_PASSWORD || "postgres",
+    port: process.env.PG_PORT || 5432,
+  });
+
+  pool.query(`
+  CREATE TABLE IF NOT EXISTS socket_io_attachments (
+      id          bigserial UNIQUE,
+      created_at  timestamptz DEFAULT NOW(),
+      payload     bytea
+  );
+`);
+
+  io.adapter(createAdapter(pool));
+}
+
 initReporting(io);
-httpServer.listen(PORT);
 io.on("connection", (socket) => {
+  //console.log({ socket });
   socket.on("ping", (cb) => {
     cb("pong");
   });
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", (reason) => {
+    console.log({ reason });
     const lastToDisconnect = io.sockets.sockets.size === 0;
     if (lastToDisconnect) {
       gc();
     }
   });
 });
-
-
+httpServer.listen(PORT);
