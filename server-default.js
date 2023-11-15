1bd2c2aed13b6d61320b9599ff9ab2a70eb5f9af
Edimar Cardoso <edimarlnx@gmail.com>
2023-11-15 15:56:32 -0300

    Adding admin

--- server-default.js
+++ server-default.js
@@ -1,12 +1,25 @@
 import { Server } from "socket.io";
 import { initReporting } from "./reporting.js";
+import { createServer } from "http";
+import { instrument } from "@socket.io/admin-ui";
 
 const PORT = process.env.PORT || 3000;
+const httpServer = createServer();
 
-const io = new Server(PORT);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
 
 initReporting(io);
-
+httpServer.listen(PORT);
 io.on("connection", (socket) => {
   socket.on("ping", (cb) => {
     cb("pong");
@@ -19,3 +32,5 @@ io.on("connection", (socket) => {
     }
   });
 });
+
+
